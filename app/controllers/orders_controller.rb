b6730028827c5085edc062e575f2a1b35758e4c2
cns <consomme_onion_soup@yahoo.co.jp>
2014-05-09 19:11:09 +0900

    地のHTMLだったセレクトをERBで書くだけ

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,5 @@
 class OrdersController < ApplicationController
   before_action :set_order, only: [:show, :edit, :update, :destroy]
-
   # GET /orders
   # GET /orders.json
   def index
--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,12 @@
 module OrdersHelper
+
+  def item_name_price
+  	arr = []
+  	items = Item.all
+  	items.each do |item|
+      arr.push( item.name + "(" + item.price.to_s + "円" + ")" )
+  	end
+  	  return arr
+  end
+
 end
