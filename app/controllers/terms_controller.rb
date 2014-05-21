c500f5c0632725760bc79a254b193129790cdedb
cns <consomme_onion_soup@yahoo.co.jp>
2014-05-21 14:17:35 +0900

    users_controllerのscaffold消し忘れを消す

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -4,17 +4,7 @@ class TermsController < ApplicationController
   end
 
   def create
-    beginning_builder = []
-    deadline_builder = []
-    5.times do |n|
-      beginning_builder << params[:term]["beginning(#{n+1}i)"].to_i
-      deadline_builder << params[:term]["deadline(#{n+1}i)"].to_i
-    end
-
-    beginning = DateTime.new(beginning_builder[0],beginning_builder[1],beginning_builder[2],beginning_builder[3],beginning_builder[4])
-    deadline = DateTime.new(deadline_builder[0],deadline_builder[1],deadline_builder[2],deadline_builder[3],deadline_builder[4])
-
-    @term = Term.new(beginning: beginning, deadline: deadline)
+    @term = Term.new(terms_params)
     if @term.save
       redirect_to nestle_input_path, notice: '期間の設定に成功しました。'
     end
@@ -33,4 +23,8 @@ class TermsController < ApplicationController
   def destroy
   end
 
+  private
+    def terms_params
+      params.require(:term).permit(:beginning, :deadline)
+    end
 end
