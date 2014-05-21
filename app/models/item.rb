8b24785d84dfaca5a90a6cc48362b797864a7dca
cns <consomme_onion_soup@yahoo.co.jp>
2014-05-21 09:37:01 +0900

    消したことで生じたエラーを解消

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,4 +11,14 @@
 
 class Item < ActiveRecord::Base
   has_many :order_details
+
+  def self.item_count
+    arr =[]
+    i = 0
+    20.times do  # １人の人が、１種類のお茶に、注文できる最大数はとりあえず１９個としておく。
+      arr << ( ["#{i}個", i] )
+      i += 1
+    end
+    return arr
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -15,7 +15,30 @@ class Order < ActiveRecord::Base
   has_many :order_details, dependent: :destroy
   belongs_to :user
   belongs_to :time_limit
+
   enum state: [:registered, :shipped, :arrived, :exchanged]
 
   accepts_nested_attributes_for :order_details, reject_if: proc { |attributes| attributes['quantity'].to_i.zero? }
+
+  class << self
+    def name_price_quantity_sum
+      Order.registered.joins(order_details: :item).group("items.id").select("items.name, items.price, SUM(quantity)")
+    end
+
+    def price_sum
+      sum = 0
+      Order.registered.each do |order|
+        sum += Order.price_sum_of_this_order(order)
+      end
+      return sum
+    end
+
+    def price_sum_of_this_order(order)
+      sum  = 0
+      order.order_details.each do |detail|
+        sum += detail.quantity * detail.item.price
+      end
+      return sum
+    end
+  end
 end
