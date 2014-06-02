696334997e05b03d1ff49ffa807dc2b80ec3ca17
cns <consomme_onion_soup@yahoo.co.jp>
2014-06-02 11:47:39 +0900

    jQueryで動作するようにする

--- Gemfile
+++ Gemfile
@@ -23,7 +23,7 @@ gem 'coffee-rails', '~> 4.0.0'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
-gem 'turbolinks'
+#gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
@@ -53,4 +53,3 @@ end
 
 # スキーマを書く
 gem 'annotate',       group: :development
-
--- app/assets/javascripts/order.js
+++ app/assets/javascripts/order.js
@@ -1,71 +1,49 @@
-//getElementするまで共通なので切り出す
-function getPriceQuantityElements(i){
-  var index_str = 0,
-      price_id = 0,
-      quantity_id = 0,
-      price_element = 0,
-      quantity_element = 0,
-      elements = 0;
+$(function() {
+  $(".name_price_options , .quantity_options").change( function() {
+    var div = $("div.order_details_form");
+    var option = div.find("option:selected");
+    var priceAry = [];
+    var quantityAry = [];
+    var priceFil = option.filter(".name_price_options > option");
+    var quantityFil = option.filter(".quantity_options > option");
+    var tmpPrice = 0;
+    var tmpQuantity = 0;
+    var totalSum = 0;
+    var selectPearLength = 0;
+    var price_yen_regex = /\(\d+円\)/;
+    var price_regex = /\d+/;
+    var price_number_with_yen;
+    var price_number_str;
+    var quantity_regex = /\d+/;
 
-  index_str = i.toString();
-  price_id = "order_order_details_attributes_" + index_str + "_item_id";
-  quantity_id = "order_order_details_attributes_" + index_str + "_quantity";
-  price_element = document.getElementById(price_id);
-  quantity_element = document.getElementById(quantity_id);
-  elements = [price_element,quantity_element];
-  return elements;
-}
+    if( priceFil.length == quantityFil.length ){
+      selectPearLength = priceFil.length;
+    }
+    else {
+      consol.log("priceFil.length != quantityFil.length");
+      return;
+    }
+    for (var i =0; i < selectPearLength; i++){
+      (function( lockedIndex ){
+        tmpPrice = priceFil.eq(lockedIndex);
+        tmpQuantity = quantityFil.eq(lockedIndex);
+        priceAry.push(tmpPrice);
+        quantityAry.push(tmpQuantity);
+      })( i );
+    }
 
-//addEventListener
-window.onload = function(){
-  for (var i = 0; i < order_details_size; i++) {
-    elements = getPriceQuantityElements(i);
-    price_element = elements[0];
-    quantity_element = elements[1];
-    price_element.addEventListener("change", calc, false);
-    quantity_element.addEventListener("change", calc, false);
-  }
-};
+    for (var i = 0; i < selectPearLength; i++){
+      (function( lockedIndex ){
+        tmpPrice = priceAry[ lockedIndex ].text();
+        price_number_with_yen = price_yen_regex.exec(tmpPrice);
+        price_number_str = price_regex.exec(price_number_with_yen);
+        tmpQuantity = quantityAry[ lockedIndex ].text();
+        quantity_str = quantity_regex.exec(tmpQuantity);
+        totalSum += price_number_str * quantity_str;
+      })( i );
+    }
 
-//合計取得を計算する
-function calc(){
-  var sum = 0,
-      price_index = 0,
-      quantity_index = 0,
-      price_text = 0,
-      quantity_text = 0,
-      price_yen_regex = 0,
-      price_regex = 0,
-      price_number_str =0,
-      price_number = 0,
-      quantity_regex = 0,
-      quantity_str = 0,
-      quantity_number =0;
-
-  for (var i = 0; i < order_details_size; i++) {
-    price_element = getPriceQuantityElements(i)[0];
-    quantity_element = getPriceQuantityElements(i)[1];
-    price_index = price_element.selectedIndex;
-    quantity_index = quantity_element.selectedIndex;
-    price_text = price_element.options[price_index].text;
-    quantity_text = quantity_element.options[quantity_index].text;
-
-    price_yen_regex = /\(\d+円\)/;
-    price_number_with_yen = price_yen_regex.exec(price_text);
-    price_regex = /\d+/;
-    price_number_str = price_regex.exec(price_number_with_yen);
-    price_number = Number(price_number_str);
-
-    quantity_regex = /\d+/;
-    quantity_str = quantity_regex.exec(quantity_text);
-    quantity_number = Number(quantity_str);
-    sum += price_number * quantity_number;
-  }
-  total = sum.toString();
-  total = '合計：' + total + '円';
-  document.getElementById("order_create_price_sum").textContent = total;
-}
-
-
-
+    $( "#order_create_price_sum" ).text("合計: "+ totalSum + "円" );
+  });
+});
 
