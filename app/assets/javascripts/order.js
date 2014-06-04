15d206b314e9685325c9bfbdf5e893db8d8b974e
cns <consomme_onion_soup@yahoo.co.jp>
2014-06-04 10:54:26 +0900

    order.jsのコーディングスタイルミスの修正

--- app/assets/javascripts/order.js
+++ app/assets/javascripts/order.js
@@ -1,6 +1,5 @@
-$(function(){
+$(function() {
   $('.name_price_options , .quantity_options').change(function() {
-
     //選択したoptionを、priceとquantityでわけて取り出す
     var selectedOptions = function() {
       var $div = $('div.order_details_form'),
@@ -10,61 +9,65 @@ $(function(){
         price:     $selectedBothOptions.filter('.name_price_options > option'),
         quantity:  $selectedBothOptions.filter('.quantity_options > option')
       };
-    }() ,
+    }(),
 
-    //正規表現で処理する前の、生(crude)のpriceとquantityを返す
-    crudeOptionContents = function(){
-      var prices =[],
-          quantities = [];
+        //正規表現で処理する前の、生(crude)のpriceとquantityを返す
+        crudeOptionContents = function() {
+          var prices =[],
+              quantities = [];
 
-      selectedOptions.price.each(function(index, options) {
-        prices[index] = $(options).text();
-      });
-      selectedOptions.quantity.each(function(index, options) {
-        quantities[index] = $(options).text();
-      });
-      return {
-        price:     prices ,
-        quantity:  quantities
-      };
-    }() ,
-
-    // 価格と個数の数字だけをとりだす。
-    numberOptionContents = function() {
-      var tmp ,
-          priceNumbers = crudeOptionContents.price.map(function(price) {
-            tmp = (/\(\d+円\)/).exec(price);
-            return (/\d+/).exec(tmp);
+          selectedOptions.price.each(function(index, options) {
+            prices[index] = $(options).text();
           });
-      var quantityNumbers = crudeOptionContents.quantity.map(function(quantity) {
-            return (/\d+/).exec(quantity);
+          selectedOptions.quantity.each(function(index, options) {
+            quantities[index] = $(options).text();
           });
-      return {
-        price:    priceNumbers ,
-        quantity: quantityNumbers
-      };
-    }() ,
 
-    //Pairの数を返す。１Pairは１つのpriceと１つのquantityのoptionからなる。
-    selectPairLength = function() {
-      if(selectedOptions.price.length === selectedOptions.quantity.length) {
-        return selectedOptions.price.length;
-      } else {
-        alert('selectedOptions.price.length != selectedOptions.quantity.length');
-        return 0;
-      }
-    }() ,
+          return {
+            price:     prices ,
+            quantity:  quantities
+          };
+        }(),
 
-    //合計を計算して、ページに反映する。
-    updateTotalSum = function() {
-      var i ,
-          sum = 0;
-      for (i = 0; i < selectPairLength; i++) {
-        (function(lockedIndex) {
-          sum += numberOptionContents.price[lockedIndex] * numberOptionContents.quantity[lockedIndex];
-        })(i);
-      }
-      $('#order_create_price_sum').text('合計: '+ sum + '円');
-    }();
+        // 価格と個数の数字だけをとりだす。
+        numberOptionContents = function() {
+          var tmp,
+              priceNumbers = crudeOptionContents.price.map(function(price) {
+                tmp = (/\(\d+円\)/).exec(price);
+                return (/\d+/).exec(tmp);
+              });
+          var quantityNumbers = crudeOptionContents.quantity.map(function(quantity) {
+                return (/\d+/).exec(quantity);
+              });
+
+          return {
+            price:    priceNumbers ,
+            quantity: quantityNumbers
+          };
+        }(),
+
+        //Pairの数を返す。１Pairは１つのpriceと１つのquantityのoptionからなる。
+        selectPairLength = function() {
+          if(selectedOptions.price.length === selectedOptions.quantity.length) {
+            return selectedOptions.price.length;
+          } else {
+            alert('selectedOptions.price.length != selectedOptions.quantity.length');
+            return 0;
+          }
+        }(),
+
+        //合計を計算して、ページに反映する。
+        updateTotalSum = function() {
+          var i,
+              sum = 0,
+              calcSum = function (lockedIndex) {
+                sum += numberOptionContents.price[lockedIndex] * numberOptionContents.quantity[lockedIndex];
+              };
+
+          for (i = 0; i < selectPairLength; i++) {
+            calcSum(i);
+          }
+          $('#order_create_price_sum').text('合計: '+ sum + '円');
+        }();
   });
 });
