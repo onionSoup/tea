21561ab1b059e820db2cea278d80be7faad908e5
cns <consomme_onion_soup@yahoo.co.jp>
2014-06-04 09:16:04 +0900

    jqueryでorder.jsを書き直す

--- app/assets/javascripts/order.js
+++ app/assets/javascripts/order.js
@@ -1,46 +1,70 @@
-$(function() {
+$(function(){
   $('.name_price_options , .quantity_options').change(function() {
-    var div = $('div.order_details_form'),
-        selectedOptions = div.find('option:selected'),
-        priceAry = [],
-        quantityAry = [],
-        priceOptions = selectedOptions.filter('.name_price_options > option'),
-        quantityOptions = selectedOptions.filter('.quantity_options > option'),
-        totalSum = 0,
-        selectPairLength = 0;
-    if(priceOptions.length == quantityOptions.length) {
-      selectPairLength = priceOptions.length;
-    } else {
-      console.log('priceOptions.length != quantityOptions.length');
-      return;
-    }
-    for (var i =0; i < selectPairLength; i++) {
-      (function(lockedIndex) {
-        var tmpPrice = 0,
-            tmpQuantity = 0;
-        tmpPrice = priceOptions.eq(lockedIndex);
-        tmpQuantity = quantityOptions.eq(lockedIndex);
-        priceAry[lockedIndex] = tmpPrice;
-        quantityAry[lockedIndex] = tmpQuantity;
-      })(i);
-    }
-    for (var j = 0; j < selectPairLength; j++) {
-      (function(lockedIndex) {
-        var priceJapanese = priceAry[lockedIndex].text(),
-            quantityJapanese = quantityText = quantityAry[lockedIndex].text(),
-            priceYenRegex = /\(\d+円\)/,
-            priceRegex = /\d+/,
-            priceNumberWithYen,
-            priceNumberStr,
-            quantityStr,
-            quantityRegex = /\d+/;
-        priceNumberWithYen = priceYenRegex.exec(priceJapanese);
-        priceNumberStr = priceRegex.exec(priceNumberWithYen);
-        quantityStr = quantityRegex.exec(quantityJapanese);
-        totalSum += priceNumberStr * quantityStr;
-      })(j);
-    }
-    $('#order_create_price_sum').text('合計: '+ totalSum + '円');
+
+    //選択したoptionを、priceとquantityでわけて取り出す
+    var selectedOptions = function() {
+      var $div = $('div.order_details_form'),
+          $selectedBothOptions = $div.find('option:selected');
+
+      return {
+        price:     $selectedBothOptions.filter('.name_price_options > option'),
+        quantity:  $selectedBothOptions.filter('.quantity_options > option')
+      };
+    }() ,
+
+    //正規表現で処理する前の、生(crude)のpriceとquantityを返す
+    crudeOptionContents = function(){
+      var prices =[],
+          quantities = [];
+
+      selectedOptions.price.each(function(index, options) {
+        prices[index] = $(options).text();
+      });
+      selectedOptions.quantity.each(function(index, options) {
+        quantities[index] = $(options).text();
+      });
+      return {
+        price:     prices ,
+        quantity:  quantities
+      };
+    }() ,
+
+    // 価格と個数の数字だけをとりだす。
+    numberOptionContents = function() {
+      var tmp ,
+          priceNumbers = crudeOptionContents.price.map(function(price) {
+            tmp = (/\(\d+円\)/).exec(price);
+            return (/\d+/).exec(tmp);
+          });
+      var quantityNumbers = crudeOptionContents.quantity.map(function(quantity) {
+            return (/\d+/).exec(quantity);
+          });
+      return {
+        price:    priceNumbers ,
+        quantity: quantityNumbers
+      };
+    }() ,
+
+    //Pairの数を返す。１Pairは１つのpriceと１つのquantityのoptionからなる。
+    selectPairLength = function() {
+      if(selectedOptions.price.length === selectedOptions.quantity.length) {
+        return selectedOptions.price.length;
+      } else {
+        alert('selectedOptions.price.length != selectedOptions.quantity.length');
+        return 0;
+      }
+    }() ,
+
+    //合計を計算して、ページに反映する。
+    updateTotalSum = function() {
+      var i ,
+          sum = 0;
+      for (i = 0; i < selectPairLength; i++) {
+        (function(lockedIndex) {
+          sum += numberOptionContents.price[lockedIndex] * numberOptionContents.quantity[lockedIndex];
+        })(i);
+      }
+      $('#order_create_price_sum').text('合計: '+ sum + '円');
+    }();
   });
 });
-
