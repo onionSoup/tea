7b8316d35c4829318498d97a7ba0ba78672d7ab5
kbaba1001 <kbaba1001@gmail.com>
2014-06-06 22:18:11 +0900

    arriveds_controller.rbとarriveds/show.html.erbを掃除した

--- app/controllers/arriveds_controller.rb
+++ app/controllers/arriveds_controller.rb
@@ -1,23 +1,13 @@
 class ArrivedsController < ApplicationController
   def show
+    @orders = Order.includes(:user, {order_details: :item}).arrived
   end
 
   def exchange
-    checked_user_names = checked_user_names(params)
-    checked_users = User.where(name: checked_user_names)
-    checked_users.each do |user|
-      unless user.orders.update_all state: Order.states['exchanged']
-        updated_flag = true
-      end
-    flash.now[:success] = '引換したことを登録しました。' if updated_flag
-    end
+    return redirect_to action: :show unless params[:order_ids]
+
+    Order.where(id: params[:order_ids]).update_all(state: Order.states['exchanged'])
+    flash.now[:success] = '引換したことを登録しました。'
     redirect_to exchanged_path
   end
-
-  private
-    def checked_user_names(params)
-      return [] unless params[:order]
-
-      params[:order][:user_hash].map {|name, checked| name if checked == '1'}.compact
-    end
 end
