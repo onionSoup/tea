a3023e79eb027c21ce8f0959f66d41d093e5eea1
kbaba1001 <kbaba1001@gmail.com>
2014-06-06 22:28:16 +0900

    cosmetics

--- app/controllers/arriveds_controller.rb
+++ app/controllers/arriveds_controller.rb
@@ -6,7 +6,7 @@ class ArrivedsController < ApplicationController
   def exchange
     return redirect_to action: :show unless params[:order_ids]
 
-    Order.where(id: params[:order_ids]).update_all(state: Order.states['exchanged'])
+    Order.where(id: params[:order_ids]).update_all(state: Order.states[:exchanged])
     flash.now[:success] = '引換したことを登録しました。'
     redirect_to exchanged_path
   end
