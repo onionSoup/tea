7db911b7b6d1c7ad44efa5b3d5e423e86ae30f5b
Keita Urashima <ursm@ursm.jp>
2014-06-09 18:14:45 +0900

    Refactoring

--- app/controllers/registereds_controller.rb
+++ app/controllers/registereds_controller.rb
@@ -8,10 +8,9 @@ class RegisteredsController < ApplicationController
   end
 
   def order
-    unless (Order.registered.update_all state: Order.states['ordered']) == 0
-      updated_flag = true
-    end
-    flash[:success] = 'ネスレ公式へ注文したことを登録しました。' if updated_flag
+    updated = Order.registered.update_all(state: Order.states['ordered']).nonzero?
+
+    flash[:success] = 'ネスレ公式へ注文したことを登録しました。' if updated
     redirect_to ordered_path
   end
 end
