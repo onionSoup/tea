6d6805d435ec02b54c10c4fe7eaf1d4f4c036540
cns <consomme_onion_soup@yahoo.co.jp>
2014-07-28 18:39:57 +0900

    make views

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,7 +2,9 @@ class Admin::UsersController < ApplicationController
   def index
     #今回はuserの数が小さいのでOK、だと思う
     @users = User.includes(order: {order_details: :item})
-                 .where(orders: {state: Order.states['ordered']})
+  end
+
+  def edit
   end
 
   def update
