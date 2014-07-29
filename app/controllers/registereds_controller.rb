79a73c3c2bfa27fa3f3d9f655d20c8e2c300b5da
cns <consomme_onion_soup@yahoo.co.jp>
2014-07-29 10:14:14 +0900

    ユーザーの一覧を管理者用ページから見れるようにする

--- app/controllers/registereds_controller.rb
+++ app/controllers/registereds_controller.rb
@@ -2,6 +2,10 @@ class RegisteredsController < ApplicationController
   def show
     @registereds = Order.registered.select_name_and_price_and_sum_of_quantity
 
+    @users = User.includes(order: {order_details: :item})
+                 .order_in_state_of('registered')
+                 .has_at_least_one_detail
+
     @total_sum = @registereds.inject(0) {|acc, order|
       acc + order.quantity * order.then_price
     }
