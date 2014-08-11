f2ffc0578ca655c4f67b68e6c8db94ea492a0bf1
cns <consomme_onion_soup@yahoo.co.jp>
2014-08-11 11:42:48 +0900

    /views/application/_admin_orders_table

--- spec/features/change_user_name_by_admin_spec.rb
+++ spec/features/change_user_name_by_admin_spec.rb
@@ -30,13 +30,17 @@ feature 'ユーザー名を管理者画面から変更' do
       expect(page).to have_content '変更前と同じ名前です'
     end
 
-    scenario '無効な名前を入力した時、ユーザー登録ができない' do
+    scenario '既存の名前を入力した時、ユーザー登録ができない' do
       fill_in 'ユーザー名', with: 'Bob'
       click_button '変更する'
-      expect(page).to have_content 'その名前は既に使われています。'
 
+      expect(page).to have_content 'その名前は既に使われています。'
+    end
+
+    scenario '無効な名前を入力した時、ユーザー登録ができない' do
       fill_in 'ユーザー名', with: ''
       click_button '変更する'
+
       expect(page).to have_content '名前が入力されていません。'
     end
   end
