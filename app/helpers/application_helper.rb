54565320fd8baaf65e58411015351b72f4f6e512
cns <consomme_onion_soup@yahoo.co.jp>
2014-08-12 13:29:59 +0900

    ApplicationHelper#render_form_errors → render partial: 'application/form_errors'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,4 @@
 module ApplicationHelper
-  def render_form_errors(object)
-    return if object.nil? || object.errors.empty?
-    render inline: <<-'EOM', type: :erb, locals: {object: object}
-  <ul id= "error_message_ul">
-    <% object.errors.values.flatten.each do |msg| %>
-      <li class="error_message_li"><%= msg %></li>
-    <% end %>
-  </ul>
-    EOM
-  end
-
   def path_to_admin_user_detail(user_id, detail_id)
     "/admin/users/#{user_id}/order_details/#{detail_id}:id"
   end
