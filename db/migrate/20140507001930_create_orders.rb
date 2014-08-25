661dd31638aa97c1ad67b8a54a25504bf36736f2
onion <=>
2014-08-25 10:53:49 +0900

    change migration

--- db/migrate/20140507001930_create_orders.rb
+++ db/migrate/20140507001930_create_orders.rb
@@ -2,9 +2,10 @@ class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
       t.integer  :user_id
-      t.datetime :created_at
-      t.datetime :updated_at
+      t.datetime :start_time
+      t.datetime :end_time
       t.integer  :state, default: 0
+      t.timestamps
     end
   end
 end
--- db/migrate/20140507002008_create_details.rb
+++ db/migrate/20140507002008_create_details.rb
@@ -1,10 +1,11 @@
-class CreateOrderDetails < ActiveRecord::Migration
+class CreateDetails < ActiveRecord::Migration
   def change
-    create_table :order_details do |t|
+    create_table :details do |t|
       t.integer :order_id
       t.integer :item_id
       t.integer :quantity
       t.integer :then_price
+      t.boolean :is_exchanged
       t.timestamps
     end
   end
