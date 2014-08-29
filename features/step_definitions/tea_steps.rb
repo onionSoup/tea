8d39f19b26c2201d6e25a2d734c5dbba3806f64b
onionSoup <consomme_onion_soup@yahoo.co.jp>
2014-08-29 10:58:34 +0900

    商品ごとに集計した表のstep

--- features/step_definitions/tea_steps.rb
+++ features/step_definitions/tea_steps.rb
@@ -13,14 +13,18 @@ Given /^ユーザー"(.*?)"が登録してログインしている$/ do |user_na
   create_user_and_login_as user_name
 end
 
-Given /^お茶"(.*?)"を数量"(.*?)"個注文している$/ do |item_name, quantity|
+Given /^"(.*?)"円のお茶"(.*?)"を数量"(.*?)"個注文している$/ do |price, item_name, quantity|
+  price_in_db = Item.find_by(name: item_name).price
+  raise "price of #{item_name} must be #{price_in_db}. not given price #{price}" unless  price_in_db == price.to_i
+
   choose_item_and_quantity(item_name, quantity)
   click_button '追加する'
 end
 
 When /^ユーザー"(.*?)"がログアウトする$/ do |user_name|
-  if current_user == User.find_by(name: user_name)
-    click_button 'ログアウト'
+  login_user_name = get_user_name_from_header!
+  if login_user_name == User.find_by(name: user_name).name
+    click_link 'ログアウト'
   else
     raise "before logout, #{user_name} must be logged in"
   end
@@ -53,6 +57,10 @@ When /^品名"(.*?)"の削除リンクをクリックする$/ do |item_name|
   end
 end
 
+When /^"(.*?)"のリンクをクリックする$/ do |link_text|
+  click_link link_text
+end
+
 # FIXME
 #本当は「明細表のｘ行目のｙはｚであること」みたいにしたい。
 #でもそれだとorder_detailを消した時、n行目の行がid n であるという対応が崩れて間違いになる。
@@ -68,11 +76,27 @@ end
 Then /^明細表が以下になること$/ do |table|
   table.hashes.each do |row|
     id = OrderDetail.find_by(item:  Item.find_by(name: row['品名'])).id
-    row.keys.each do |key|
-      step %Q{"#{id}"番目に作った明細は、明細表で"#{key}"が"#{row[key]}"であること}
+    row.keys.each do |col_name|
+      step %Q{"#{id}"番目に作った明細は、明細表で"#{col_name}"が"#{row[col_name]}"であること}
     end
   end
 end
+
+Then /商品ごとに集計した表が以下と等しいこと$/ do |table|
+  table.hashes.each do |row|
+    row.keys.each do |col_name|
+      within('.all_user_sum_table') do
+        within(".item_#{Item.find_by(name: row['商品名']).id}") do
+          td_text = find(to_class_name_admin_order_table(col_name)).text
+          expect(td_text).to eq row[col_name]
+        end
+      end
+    end
+  end
+end
+
+
+
 
 Then /^"(.*?)"と表示されていること$/ do |content|
   expect(page).to have_content content
@@ -92,7 +116,31 @@ module StepDefinitionsUtil
       '.sum'
     when ''
       '.destroy'
+    else
+      raise "pass invalid arg '#{col_name}' to #{__method__}"
     end
+  end
+
+  def to_class_name_admin_order_table(col_name)
+    case col_name
+    when '商品名'
+      '.name'
+    when '単価'
+      '.price'
+    when '合計数量'
+      '.sum'
+    else
+      raise "pass invalid arg '#{col_name}' to #{__method__}"
+    end
+  end
+
+  def get_user_name_from_header!
+    within('header') do
+      match_data = find('#greeting').text.match /^こんにちは(.*?)さん$/
+      match_data[1]
+    end
+  rescue
+    raise 'ヘッダーから名前を取得できません。'
   end
 end
 include StepDefinitionsUtil
