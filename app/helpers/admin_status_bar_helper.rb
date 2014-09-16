4d73045ebd0d01c7af074ce23afed6f3493fc7a3
onionSoup <consomme_onion_soup@yahoo.co.jp>
2014-09-16 18:06:58 +0900

    admin dashboard paragraph

--- app/helpers/admin_status_bar_helper.rb
+++ app/helpers/admin_status_bar_helper.rb
@@ -25,27 +25,25 @@ module AdminStatusBarHelper
     explain = case Order.admin_status_with_period
     when 'undefined_period'
       [cart_position_names[0],
-      "#{link_to '注文期間の設定', admin_period_path}をしてください。<br>注文期間を設定することで、ユーザーからの注文を募集でき、次の「注文待ち」に移ります。"]
+      '現在、注文期間が未設定です。ユーザーが注文できません。']
     when 'can_add_detail'
       [cart_position_names[1],
-    "ユーザーが注文できます。<br>ユーザーの注文は#{link_to 'ネスレ入力用ページ', registered_path}で確認できます。<p>注文期間が切れる#{l(Period.singleton_instance.end_time + 1.days)}以降は、「発注」または「注文なし」に移ります。</p>"]
+      "#{l Period.singleton_instance.end_time}まで注文期間中です。ユーザーが注文できます。"]
     when 'wait_ordered'
       [cart_position_names[2],
-      "ネスレに発注ができます。<br>#{link_to 'ネスレ入力用ページ', registered_path}で確認できるユーザーの注文を、#{about_page_link}で注文してください。<br>発注後、#{link_to 'ネスレ入力用ページ', registered_path}でボタンを押せば、次の「発送待ち」に移ります。
-        "]
+      '注文期間が終了しました。期間中にあったユーザーからの注文を、まだネスレに発注していません。']
     when 'nothing_added'
       [cart_position_names[6],
-      "１人のユーザーも注文せずに注文期間が終了しました。<br>#{link_to '注文期間の削除', admin_period_path}をして新しく注文を受け付けることができます。"]
+      '１人のユーザーも注文せずに注文期間が終了しました。']
     when 'ordered'
       [cart_position_names[3],
       '注文は発送されました。支社に届くのを待っています。']
     when 'arrived'
       [cart_position_names[4],
-      "注文したお茶が支社に届きました。<p>引換を完了していないユーザーがいます。<br>お金を渡してお茶を受け取ってください。</p>その後、#{link_to '引換用ページ', arrived_path}で引換したことをシステムに記録してください。"]
+      '注文したお茶が支社に届きました。']
     when 'finish'
       [cart_position_names[5],
-      "すべてのユーザーの引換が完了しました。<br>現在
-      、ユーザーの注文を募集していません。<br>#{link_to '注文期間の削除', admin_period_path}をして新しく注文を受け付けることができます。"]
+      "すべてのユーザーの引換が完了しました。<br>現在、ユーザーの注文を募集していません。"]
     else
       raise "bag in #{__method__}"
     end
@@ -71,11 +69,15 @@ module AdminStatusBarHelper
     when 'nothing_added'
       "#{link_to '注文期間の削除', admin_period_path}をして新しく注文を受け付けることができます。<br>注文期限を削除すれば「期間設定」に移ります。"
     when 'ordered'
-      "お茶が支社に届いた場合、#{link_to '発送待ち商品の確認ページ'}でボタンを押してください。<br>引換ができるようになり、次の「引換」に移ります。"
+      "お茶が支社に届いた場合、#{link_to '発送待ち商品の確認ページ', ordered_path}でボタンを押してください。<br>引換ができるようになり、次の「引換」に移ります。"
     when 'arrived'
-      "お茶とお金の引換ができます。<br>#{link_to '引換用ページ', arrived_path}で注文したユーザーを確認してください。<br>
-      お金を渡してお茶を受け取り、引換用ページでボタンを押して<br>引換をシステムに記録してください。</p>
-      <p>すべての引換が終了すれば、次の「終了処理」に移ります。"
+      "お茶とお金の引換ができます。以下の手順で作業ができます。
+      <ol>
+        <li>#{link_to '引換用ページ', arrived_path}で引換できるお茶とユーザーを確認してください。</li>
+        <li>お金を受け取ってお茶を渡してください。（引換してください。）</li>
+        <li>#{link_to '引換用ページ', arrived_path}で引換た人の分にチェックを入れ<br>ボタンを押して引換をシステムに記録してください。</li>
+        <li>すべての人の分について、引換が終了すれば、次の「終了処理」に移ります。</li>
+      </ol>"
     when 'finish'
       "#{link_to '注文期間の削除', admin_period_path}をして新しく注文を受け付けることができます。<br>
       注文期間の削除後は、「期間設定」に移ります。"
