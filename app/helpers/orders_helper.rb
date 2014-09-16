8bdaed07a517d3dbbe9c5e4ef6b0485b4dbda8e1
onionSoup <consomme_onion_soup@yahoo.co.jp>
2014-09-16 16:39:09 +0900

    take out the garbage

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -16,22 +16,4 @@ module OrdersHelper
 
     return index_link
   end
-
-=begin
-  def link_to_index_or_show(index_link_text, show_link_text)
-    index_link  = content_tag(:a, href: order_details_path) { "#{index_link_text}" }
-    period_link = content_tag(:a, href: period_notice_path) { "#{index_link_text}" } #これはミスではない。リンクテキストは使いまわす。
-    show_link   = content_tag(:a, href: order_path)         { "#{show_link_text}" }
-
-
-    return period_link if Period.has_undefined_times?
-
-    if Period.out_of_date?
-      return index_link if current_user.order.registered? && current_user.order.order_details.empty?
-      return show_link
-    end
-
-    index_link
-  end
-=end
 end
