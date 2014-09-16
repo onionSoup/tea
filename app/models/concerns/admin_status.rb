59772242575310a1378ffc5f1ba6dd206104ac7f
onionSoup <consomme_onion_soup@yahoo.co.jp>
2014-09-16 15:40:20 +0900

    create admin_order_state_bar

--- app/models/concerns/admin_status.rb
+++ app/models/concerns/admin_status.rb
@@ -1,6 +1,9 @@
 module AdminStatus
   def admin_cart_position_names(nothing_added: false)
     if nothing_added
+      #['期間設定', '注文待ち', '発 注 ', '発送待ち', '引 換 ', '終了処理', '注文なし']
+      %w(期間設定 注文待ち 発注 発送待ち 引換  終了処理 注文なし)
+
       %w(期間設定 注文待ち 発注 発送待ち 引換  終了処理 注文なし)
     else
       %w(期間設定 注文待ち 発注 発送待ち 引換 終了処理)
@@ -22,6 +25,6 @@ module AdminStatus
 
     return 'ordered' if Order.all.any? {|order| order.ordered? }
     return 'arrived' if Order.all.any? {|order| order.arrived? }
-    return 'finish'  if Order.all.all? {|order| order.anexchanged? || order.empty_order? }
+    return 'finish'  if Order.all.all? {|order| order.exchanged? || order.empty_order? }
   end
 end
